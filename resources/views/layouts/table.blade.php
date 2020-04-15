95740f2a19911bd6d3fb6bbb9e878c630286caaf
Tech Lord <techlord001@protonmail.com>
2020-04-15 10:16:57 +0100

    table layout enhanced, image thumbnails and additional columns added

--- resources/views/layouts/table.blade.php
+++ resources/views/layouts/table.blade.php
@@ -7,7 +7,33 @@
         <table class="table table-hover">
             <thead>
                 <tr>
+                    <th class="text-center">
+                        @switch(Request::url())
+                            @case(Request::is('labels'))
+                                Logo
+                                @break
+                            @case(Request::is('artists'))
+                                Portrait
+                                @break
+                            @case(Request::is('records'))
+                                Cover
+                                @break
+                            @default
+                                -
+                        @endswitch
+                    </th>
                     <th>Name</th>
+                    @if (Request::is('records'))
+                        <th>Artist</th>
+                    @endif
+                    @if (Request::is('artists') || Request::is('records'))
+                    <th>Label</th>
+                    @endif
+                    @if (Request::is('records'))
+                        <th class="text-center">Format</th>
+                        <th class="text-center">Colour</th>
+                        <th class="text-center">Released</th>
+                    @endif
                     <th class="text-right">Options</th>
                     @auth
                         <th class="text-right">Remove</th>
@@ -19,10 +45,17 @@
                     @case(Request::is('labels'))
                         @forelse ($labels as $label)
                             <tr>
+                                <td class="align-middle text-center">
+                                    @if ($label->image)
+                                        <img src="{{ asset('storage/' . $label->image) }}" alt="" class="img-thumbnail rounded table-item-thumbnail">
+                                    @else
+                                        -
+                                    @endif
+                                </td>
                                 <td>{{ $label->name }}</td>
                                 <td class="text-right">
                                     <a href="/labels/{{ $label->id }}">
-                                        <button type="button" class="btn btn-secondary btn-sm">Show</button>
+                                        <button type="button" class="btn btn-secondary btn-sm">Details</button>
                                     </a>
                                     @auth
                                         <a href="/labels/{{ $label->id }}/edit">
@@ -50,10 +83,24 @@
                     @case(Request::is('artists'))
                         @forelse ($artists as $artist)
                             <tr>
+                                <td class="align-middle text-center">
+                                    @if ($artist->image)
+                                        <img src="{{ asset('storage/' . $artist->image) }}" alt="" class="img-thumbnail rounded table-item-thumbnail">
+                                    @else
+                                        -
+                                    @endif
+                                </td>
                                 <td>{{ $artist->name }}</td>
+                                <td>
+                                    @if ($artist->label_id)
+                                        {{ $artist->label->name }}
+                                    @else
+                                        N/A
+                                    @endif
+                                </td>
                                 <td class="text-right">
                                     <a href="/artists/{{ $artist->id }}">
-                                        <button type="button" class="btn btn-secondary btn-sm">Show</button>
+                                        <button type="button" class="btn btn-secondary btn-sm">Details</button>
                                     </a>
                                     @auth
                                         <a href="/artists/{{ $artist->id }}/edit">
@@ -81,10 +128,50 @@
                     @case(Request::is('records'))
                         @forelse ($records as $record)
                             <tr>
-                                <td>{{ $record->name }}</td>
-                                <td class="text-right">
+                                <td class="align-middle text-center">
+                                    @if ($record->image)
+                                        <img src="{{ asset('storage/' . $record->image) }}" alt="" class="img-thumbnail rounded table-item-thumbnail">
+                                    @else
+                                        -
+                                    @endif
+                                </td>
+                                <td class="align-middle">
+                                    {{ $record->name }}
+                                </td>
+                                <td class="align-middle">
+                                    {{ $record->artist->name }}
+                                </td>
+                                <td class="align-middle">
+                                    @if ($record->artist->label)
+                                        {{ $record->artist->label->name }}
+                                    @else
+                                        -
+                                    @endif
+                                </td>
+                                <td class="text-center align-middle">
+                                    @if ($record->format_id)
+                                        {{ $record->format->format }}
+                                    @else
+                                        -
+                                    @endif
+                                </td>
+                                <td class="text-center align-middle">
+                                    @if ($record->colour_id)
+                                        {{ $record->colour->colour }}
+                                    @else
+                                        -
+                                    @endif
+                                </td>
+                                <td class="text-center align-middle">
+                                    @if ($record->released)
+                                        {{ $record->released }}
+                                    @else
+                                        -
+                                    @endif
+                                </td>
+                                <td class="text-right align-middle">
                                     <a href="/records/{{ $record->id }}">
-                                        <button type="button" class="btn btn-secondary btn-sm">Show</button>
+                                        <button type="button" class="btn btn-secondary btn-sm">Details</button>
                                     </a>
                                     @auth
                                         <a href="/records/{{ $record->id }}/edit">
@@ -93,7 +180,7 @@
                                     @endauth
                                 </td>
                                 @auth
-                                    <td class="text-right">
+                                    <td class="text-right align-middle">
                                         <form action="/records/{{ $record->id }}" method="post">
                                             @method('delete')
                                             @csrf
@@ -104,7 +191,7 @@
                             </tr>
                         @empty
                             <tr>
-                                <td>No records!</td>
+                                <td class="align-middle">No records!</td>
                             </tr>
                         @endforelse
                         
